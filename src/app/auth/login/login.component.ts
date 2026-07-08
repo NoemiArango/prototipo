e4636581cc80a77cc768f9104bef6ed6165f575f
intmimito01 <noemiarango6@gmail.com>
2026-07-08 14:08:10 -0600

    login

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,44 @@
+import { Component, signal, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+
+  showPassword = signal(false);
+  loading = signal(false);
+  errorMsg = signal('');
+
+  form = this.fb.group({
+    usuario: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(4)]]
+  });
+
+  togglePassword() {
+    this.showPassword.update(v => !v);
+  }
+
+  onSubmit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    this.loading.set(true);
+    this.errorMsg.set('');
+
+    setTimeout(() => {
+      this.loading.set(false);
+      this.router.navigate(['/dashboard']);
+    }, 800);
+  }
+}
